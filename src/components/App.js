a24f36f979e96785b0aba4e75668876f610462dc
Alan Medina <realconect@gmail.com>
2019-02-24 21:47:36 -0600

    Modified the default text in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Hello from "./Hello";
 import "../styles/main.scss";
 
 class App extends React.Component {
   render() {
     return (
       <div id="app">
-        <h1>Hello world from Webpack and React!</h1>
-        <Hello name="Alan" />
-        <hr />
-        <p>This is the App component</p>
+        <p>Put your content here</p>
       </div>
     );
   }
